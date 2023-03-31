96548d6439006d8f28a249aa11114cce257189b4
ddansab <ddansab@gmail.com>
2023-03-30 19:45:38 -0700

    moved stuff to gitignore

--- flashcard_maker.py
+++ flashcard_maker.py
@@ -15,25 +15,25 @@ translator = Translator()
 def introStuff(fileName, categoryName):
     num = random.randrange(1, 100)
     if not categoryName:
-        w = open('{}_pleco.txt'.format(fileName), 'w')
+        w = open('new_files/{}_pleco.txt'.format(fileName), 'w')
         w.write("//{} Monkey Butts\n".format(num))
     else:
-        w = open('{}_pleco.txt'.format(fileName), 'w')
+        w = open('new_files/{}_pleco.txt'.format(fileName), 'w')
         w.write("//{}\n".format(categoryName))
 
 def zhengLongShuo():
-   with open(r'starter_file.txt', 'r') as werdz:
+   with open(r'starter/starter_file.txt', 'r') as werdz:
       data = werdz.read()
       data = data.replace(", ", "\n")
 
-   with open(r'starter_file.txt', 'w') as werdz:
+   with open(r'starter/starter_file.txt', 'w') as werdz:
       werdz.write(data)
 
 
 zhengLongShuo()
 
 # get the characters from file
-with open('starter_file.txt') as werdz:
+with open('starter/starter_file.txt') as werdz:
     lines = werdz.readlines()
 
 # loop through the file
@@ -47,7 +47,7 @@ def getTheWerdz():
         transLF = translator.translate(transSF, dest='zh-tw')
         transPY = translator.translate(line, dest='zh-cn')
     
-        f = open('{}_pleco.txt'.format(fName), 'a')
+        f = open('new_files/{}_pleco.txt'.format(fName), 'a')
         f.write("{}[{}]\t{}\t{}\n".format(transSF, transLF.text, transPY.pronunciation, transEN.text))
     
        	print(wait)
@@ -59,6 +59,6 @@ def doTheThing():
 
 doTheThing()
 print("==========================")
-print("All done! You can find `{}_pleco.txt` inside your {} folder.".format(fName, Path.cwd()))
+print("All done! You can find `{}_pleco.txt` inside your {}/new_files folder.".format(fName, Path.cwd()))
 print("Just Teams or email it to yourself, and upload it into Pleco.")
 print("=========================")
